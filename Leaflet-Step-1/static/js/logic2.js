25dff2473156c05e6c8f1f570c0fcbedec1073b0
Douglas Krug <dkrug82@gmail.com>
2021-07-21 14:27:46 -0400

    created new logic js and completed step 1

---
+++ Leaflet-Step-1/static/js/logic2.js
@@ -0,0 +1,88 @@
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(url).then(function (data) {
+
+    console.log(data);
+});
+
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+});
+
+// Create the map object with options
+var map = L.map("map", {
+    center: [37.09, -95.7129],
+    zoom: 5,
+    //layers: [lightmap, earthquakes]
+});
+
+lightmap.addTo(map);
+
+d3.json(url).then(function (data) {
+    function createMarkers(feature) {
+        return {
+
+            radius: radius(feature.properties.mag),
+            fillColor: color(feature.geometry.coordinates[2]),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }
+    }
+
+    function radius(mag) {
+        return mag * 4;
+    }
+
+    function color(depth) {
+
+        switch (true) {
+            case depth > 90:
+                return "red";
+            case depth > 70:
+                return "darkorange";
+            case depth > 50:
+                return "orange";
+            case depth > 30:
+                return "yellow";
+            case depth > 10:
+                return "greenyellow";
+            default:
+                return "green";
+        }
+    }
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: createMarkers,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude " + feature.properties.mag + "<br>" + feature.properties.place)
+        }
+    }).addTo(map);
+
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function () {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 50, 70, 90],
+            colors = ["green", "greenyellow", "yellow", "orange", "darkorange", "red"];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<ui style="background:' + color(grades[i] + 1) + '">&nbsp&nbsp</ui> ' +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    legend.addTo(map);
+})
+
